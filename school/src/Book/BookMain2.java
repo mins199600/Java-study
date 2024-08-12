f31fa0b2bf5ed37af72c9760dfcc2c51a672a026
sumin <mins19960091@gmail.com>
2024-08-12 11:16:49 +0900

    List 와 Set 에 대한 내용 공부

--- school/src/Book/BookMain2.java
+++ school/src/Book/BookMain2.java
@@ -7,13 +7,13 @@ public class BookMain2 {
 		System.out.println(book.page); //0
 		System.out.println(book.title);//null
 		
-		Book book1 = new Book(33);
+		Book book1 = new Book();
 		System.out.println(book1.author); //null
 		System.out.println(book1.page); //33
 		System.out.println(book1.title);//null
 		
 		System.out.println("=========================================");
-		Book book3 = new Book("이수민", 500, "원피스");
+		Book book3 = new Book();
 		System.out.println(book3.author); //이수민
 		System.out.println(book3.page); //500
 		System.out.println(book3.title);//원피스
---
+++ school/src/list/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		String[] arrayObj = new String[2];
+		arrayObj[0] = "one";
+		arrayObj[1] = "two";
+	//	arrayObj[2] = "three";	//오류가 발생한다
+		
+		for(int i = 0; i<arrayObj.length; i++) {
+			System.out.println(arrayObj[i]);
+		}
+	
+	
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("one");
+		al.add("two");
+		al.add("three");
+		
+		for(int i = 0; i<al.size(); i++) {
+			String value = al.get(i);
+			System.out.println(value);
+		}
+	}
+
+}
